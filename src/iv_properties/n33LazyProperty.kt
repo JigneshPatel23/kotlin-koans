6a5526e79f1605d1a07892c3c46e70c3417cd81d
Jignesh Patel <jignesh.patel@jiniguru.net>
2017-10-12 15:57:04 +0530

    Resolve task 33

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -3,7 +3,11 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private val lazyVal : Int? = null
+        get() = if(field == null) {field = initializer(); field} else field
+
+    val lazy: Int
+        get() = lazyVal!!
 }
 
 fun todoTask33(): Nothing = TODO(
