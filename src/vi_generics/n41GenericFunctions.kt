7a0c7a6f2254887701646439b391873226dc56a2
Jignesh Patel <jignesh.patel@jiniguru.net>
2017-10-13 15:33:48 +0530

    Resolve task 41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -2,6 +2,7 @@ package vi_generics
 
 import util.TODO
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun task41(): Nothing = TODO(
     """
@@ -20,11 +21,31 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+}
+
+fun List<String>.partitionTo(first: MutableList<String>, second: MutableList<String>, predicate: (String) -> Boolean) : Pair<List<String>, List<String>>{
+    for (element in this){
+        if( predicate.invoke(element) ){
+            first.add(element)
+        }else{
+            second.add(element)
+        }
+    }
+    return Pair(first, second)
+}
+
+fun Set<Char>.partitionTo(first: MutableSet<Char>, second: MutableSet<Char>, predicate: (Char) -> Boolean) : Pair<Set<Char>, Set<Char>>{
+    for (element in this){
+        if( predicate.invoke(element) ){
+            first.add(element)
+        }else{
+            second.add(element)
+        }
+    }
+    return Pair(first, second)
 }
